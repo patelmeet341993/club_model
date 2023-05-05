db12df5f344f459871fd286f1b355f0ae1eb7b93
Rockstar95 <62421182+Rockstar95@users.noreply.github.com>
2023-05-05 16:04:28 +0530

    added checkUserWithIdExistOrNotAndIfNotExistThenCreate method in UserController

--- lib/backend/user/user_controller.dart
+++ lib/backend/user/user_controller.dart
@@ -15,6 +15,41 @@ class UserController {
 
   UserRepository  get userRepository => _userRepository;
 
+  Future<bool> checkUserWithIdExistOrNotAndIfNotExistThenCreate({
+    required String userId,
+    String mobileNumber = "",
+  }) async {
+    String tag = MyUtils.getNewId();
+    MyPrint.printOnConsole("UserController().checkUserWithIdExistOrNotAndIfNotExistThenCreate() called with userId:'$userId', mobileNumber:'$mobileNumber'", tag: tag);
+
+    bool isUserExist = false;
+
+    if(userId.isEmpty) return isUserExist;
+
+    try {
+      UserModel? userModel = await userRepository.getUserModelFromId(userId: userId);
+      MyPrint.printOnConsole("userModel:'$userModel'", tag: tag);
+
+      if(userModel != null) {
+        isUserExist = true;
+      }
+      else {
+        UserModel createdUserModel = UserModel(
+          id: userId,
+          mobileNumber: mobileNumber,
+        );
+        bool isCreated = await userRepository.createNewUser(userModel: createdUserModel);
+        MyPrint.printOnConsole("isUserCreated:'$isCreated'", tag: tag);
+      }
+    }
+    catch(e, s) {
+      MyPrint.printOnConsole("Error in UserController().checkUserWithIdExistOrNotAndIfNotExistThenCreate():'$e'", tag: tag);
+      MyPrint.printOnConsole(s, tag: tag);
+    }
+
+    return isUserExist;
+  }
+
   Future<bool> createNewUser({required UserModel userModel}) async {
     String tag = MyUtils.getNewId();
     MyPrint.printOnConsole("UserController().createNewUser() called with userModel:'$userModel'", tag: tag);
