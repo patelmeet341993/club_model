3947a7daeb4e21db8e6f30cf1d36136f7c95f0f5
DELL <virendesaivd77@gmail.com>
2023-05-10 11:07:45 +0530

    Updated Product Model for adding brand

--- lib/models/product/data_model/product_model.dart
+++ lib/models/product/data_model/product_model.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:club_model/club_model.dart';
 
 import '../../../utils/my_utils.dart';
 import '../../../utils/parsing_helper.dart';
@@ -6,9 +7,8 @@ import '../../../utils/parsing_helper.dart';
 class ProductModel {
   String id = "";
   String name = "";
-  String brandName = "";
   String thumbnailImageUrl = "";
-  String brandThumbnailImageUrl = "";
+  BrandModel? brand;
   double sizeInML = 0;
   double price = 0;
   Timestamp? createdTime;
@@ -17,9 +17,8 @@ class ProductModel {
   ProductModel({
     this.id = "",
     this.name = "",
-    this.brandName = "",
     this.thumbnailImageUrl = "",
-    this.brandThumbnailImageUrl = "",
+    this.brand,
     this.sizeInML = 0,
     this.price = 0,
     this.createdTime,
@@ -37,26 +36,29 @@ class ProductModel {
   void initializeFromMap(Map<String, dynamic> map) {
     id = ParsingHelper.parseStringMethod(map['id']);
     name = ParsingHelper.parseStringMethod(map['name']);
-    brandName = ParsingHelper.parseStringMethod(map['brandName']);
     thumbnailImageUrl = ParsingHelper.parseStringMethod(map['thumbnailImageUrl']);
-    brandThumbnailImageUrl = ParsingHelper.parseStringMethod(map['brandThumbnailImageUrl']);
     sizeInML = ParsingHelper.parseDoubleMethod(map['sizeInML']);
     price = ParsingHelper.parseDoubleMethod(map['price']);
     createdTime = ParsingHelper.parseTimestampMethod(map['createdTime']);
     updatedTime = ParsingHelper.parseTimestampMethod(map['updatedTime']);
+
+    brand = null;
+    Map<String, dynamic> brandMap = ParsingHelper.parseMapMethod<dynamic, dynamic, String, dynamic>(map['brand']);
+    if(brandMap.isNotEmpty) {
+      brand = BrandModel.fromMap(brandMap);
+    }
   }
 
   Map<String, dynamic> toMap({bool toJson = false}) {
     return <String, dynamic>{
       "id" : id,
       "name" : name,
-      "brandName" : brandName,
       "thumbnailImageUrl" : thumbnailImageUrl,
-      "brandThumbnailImageUrl" : brandThumbnailImageUrl,
       "sizeInML" : sizeInML,
       "price" : price,
       "createdTime" : toJson ? createdTime?.toDate().millisecondsSinceEpoch : createdTime,
       "updatedTime" : toJson ? updatedTime?.toDate().millisecondsSinceEpoch : updatedTime,
+      "brand" : brand?.toMap(toJson: toJson),
     };
   }
 
