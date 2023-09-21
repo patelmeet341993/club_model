6575ed1e32ae37c52ead5aeae8fc824c715e7151
DELL <virendesaivd77@gmail.com>
2023-09-21 15:00:38 +0530

    Added Notification Provider

--- lib/backend/notification/notification_provider.dart
+++ lib/backend/notification/notification_provider.dart
@@ -1,9 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../models/common/data_model/notication_model.dart';
 import '../common/common_provider.dart';
 
 class NotificationProvider extends CommonProvider{
-  NotificationProvider(){}
+  NotificationProvider(){
+    notificationList = CommonProviderListParameter<NotificationModel>(
+      list: [],
+      notify: notify,
+    );
+    lastDocument = CommonProviderPrimitiveParameter<DocumentSnapshot<Map<String, dynamic>>?>(
+      value:null,
+      notify: notify,
+    );
+    notificationCount = CommonProviderPrimitiveParameter<int>(
+      value:0,
+      notify: notify,
+    );
+    hasMoreNotifications = CommonProviderPrimitiveParameter<bool>(
+      value:false,
+      notify: notify,
+    );
+    notificationLoading = CommonProviderPrimitiveParameter<bool>(
+      value:false,
+      notify: notify,
+    );
+  }
 
-  // late CommonProviderPrimitiveParameter<PropertyModel?> propertyModel;
+  late CommonProviderListParameter<NotificationModel> notificationList;
+  late CommonProviderPrimitiveParameter<DocumentSnapshot<Map<String, dynamic>>?> lastDocument;
+  late CommonProviderPrimitiveParameter<int> notificationCount;
+  late CommonProviderPrimitiveParameter<bool> hasMoreNotifications;
+  late CommonProviderPrimitiveParameter<bool> notificationLoading;
+
 
 
 
